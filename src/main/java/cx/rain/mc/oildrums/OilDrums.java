ca42f0eb2624a8683087a0a3982aa9991f58339d
秋雨落 <i@rain.cx>
2022-05-21 16:41:09 +0800

    init.
    Signed-off-by: 秋雨落 <i@rain.cx>

---
+++ src/main/java/cx/rain/mc/oildrums/OilDrums.java
@@ -0,0 +1,12 @@
+package cx.rain.mc.oildrums;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(OilDrums.MODID)
+public class OilDrums {
+    public static final String MODID = "oildrums";
+
+    public OilDrums() {
+        
+    }
+}
